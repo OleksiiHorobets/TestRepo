50531f0e0672d2ccea88717f196b028138c29716
oleksii.horobets <alexeyhorobets@gmail.com>
2023-06-21 19:21:47 +0300

    added sayHello with id

--- src/main/java/com/example/testproject/controller/HelloController.java
+++ src/main/java/com/example/testproject/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.testproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,14 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello!";
+    }
+
+    @GetMapping("/hello/{id}")
+    public String sayHello(@PathVariable long id) {
+        if (id < 0) {
+            return "ID is incorrect!";
+        } else {
+            return "Your request is processing!";
+        }
     }
 }
